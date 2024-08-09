39bda1afafbfec3223e0db532f5ab2580e4c61c6
rw <rw@etb>
2024-08-08 20:41:47 -0500

    Structuree 40%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,46 @@ const StatementLayer = dynamic(
   () => import("@/components").then((mod) => mod.StatementLayer),
   { ssr: true }
 );
+const AboutLayer = dynamic(
+  () => import("@/components").then((mod) => mod.AboutLayer),
+  { ssr: true }
+);
+
+const DjosLayer = dynamic(
+  () => import("@/components").then((mod) => mod.DjosLayer),
+  { ssr: true }
+);
+
+const SkillsLayer = dynamic(
+  () => import("@/components").then((mod) => mod.SkillsLayer),
+  { ssr: true }
+);
+
+const WorksLayer = dynamic(
+  () => import("@/components").then((mod) => mod.WorksLayer),
+  { ssr: true }
+);
+
+const ContactLayer = dynamic(
+  () => import("@/components").then((mod) => mod.ContactLayer),
+  { ssr: true }
+);
+
+
+
 
 
 export default function MyHome(){
 
   const view = useIntro(state => state.view)
   const {scrollContainer, scrollRight, scrollLeft} = useHorizontalScroll()
+
+  const { ref: statementRef, inView: statementInView } = useInView({triggerOnce: true});
+  const { ref: aboutRef, inView: aboutInView } = useInView({triggerOnce: true});
+  const { ref: djosRef, inView: djosInView } = useInView({triggerOnce: true});
+  const { ref: skillsRef, inView: skillsInView } = useInView({triggerOnce: true});
+  const { ref: worksRef, inView: worksInView } = useInView({triggerOnce: true});
+  const { ref: contactRef, inView: contactInView } = useInView({triggerOnce: true});
 
     return (
       <>
@@ -28,12 +62,48 @@ export default function MyHome(){
             style={{ scrollSnapType: "x mandatory" }}
           >
             <div
-              className="flex-shrink-0 h-dvh w-dvw "
+              ref={statementRef}
+              className="flex-shrink-0 h-dvh w-dvw"
               style={{ scrollSnapAlign: "start" }}
             >
-              <StatementLayer />
+              {statementInView && <StatementLayer />}
             </div>
-            
+            <div
+              ref={aboutRef}
+              className="flex-shrink-0 h-dvh w-dvw"
+              style={{ scrollSnapAlign: "start" }}
+            >
+              {aboutInView && <AboutLayer />}
+            </div>
+            <div
+              ref={djosRef}
+              className="flex-shrink-0 h-dvh w-dvw"
+              style={{ scrollSnapAlign: "start" }}>
+
+              {djosInView && <DjosLayer />}
+            </div>
+            <div
+              ref={skillsRef}
+              className="flex-shrink-0 h-dvh w-dvw"
+              style={{ scrollSnapAlign: "start" }}>
+
+              {skillsInView && <SkillsLayer />}
+            </div>
+            <div
+              ref={worksRef}
+              className="flex-shrink-0 h-dvh w-dvw"
+              style={{ scrollSnapAlign: "start" }}>
+
+              {worksInView && <WorksLayer />}
+             </div>
+            <div
+              ref={contactRef}
+              className="flex-shrink-0 h-dvh w-dvw"
+              style={{ scrollSnapAlign: "start" }}>
+
+              {contactInView && <ContactLayer />}
+              </div> 
+
           </div>
           <BtnSlide onClick={scrollLeft} isLoR="l">
             <HiArrowSmallLeft size={34} className="fill-border" />
